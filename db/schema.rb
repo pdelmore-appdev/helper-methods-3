1e9b71b4317fd07675cd9b292ce12760b991f969
Paulina Delmore <delmore.paulina@gmail.com>
2023-04-02 18:55:33 +0000

    add actor model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_02_175316) do
+ActiveRecord::Schema.define(version: 2023_04_02_185448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actors", force: :cascade do |t|
+    t.date "dob"
+    t.text "bio"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "directors", force: :cascade do |t|
     t.string "name"
